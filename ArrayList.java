83b98941e479a510a67b35834b68da81d16b2917
bhuvanthakur19x <bhuvanthakur685@gmail.com>
2025-09-26 12:33:17 +0530

    Add files via upload

---
+++ ArrayList.java
@@ -0,0 +1,14 @@
+package Array;
+import java.util.ArrayList;
+import java.util.scanner;
+
+public class ArrayList {
+
+	public static arraylist<String> addElement(Arraylist<String>list){
+		Scanner sc = new scanner(System.in);
+		
+		
+		
+	}
+
+}
